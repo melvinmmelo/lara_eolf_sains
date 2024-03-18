4a65f73d0cd7d4868e4d39c24dee27975a15b922
Melvin <melvinmmelo@gmail.com>
2024-03-18 10:19:47 +0800

    added initial routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\BranchesController;
+use App\Http\Controllers\CompanyDetailsController;
+use App\Http\Controllers\CustomersController;
 use App\Http\Controllers\ProfileController;
+use App\Models\CompanyDetails;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,9 +20,26 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit'); // views
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update'); // backend
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); // backend
+
+    // views
+    Route::get('/company', [CompanyDetailsController::class, 'index'])->name('company');
+    Route::get('/edit-company', [CompanyDetailsController::class, 'edit'])->name('company.edit');
+
+    Route::get('/branch', [BranchesController::class, 'index'])->name('branch');
+    Route::get('/edit-branch/{id}', [BranchesController::class, 'edit'])->name('branch.edit');
+
+    Route::get('/customers', [CustomersController::class, 'index'])->name('customers');
+    Route::get('/edit-customer/{id}', [CustomersController::class, 'edit'])->name('customer.edit');
+
+    Route::get('/vehicles', [CustomersController::class, 'index'])->name('vehicles');
+    Route::get('/edit-vehicle/{id}', [CustomersController::class, 'edit'])->name('vehicle.edit');
+
+    Route::get('/delivery-person', [CustomersController::class, 'index'])->name('branch');
+    Route::get('/edit-delivery-person/{id}', [CustomersController::class, 'edit'])->name('branch.edit');
+
 });
 
 require __DIR__.'/auth.php';
