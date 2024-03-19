a91c3a9e25b70119a22ae901f68bda5fcf989722
Melvin <melvinmmelo@gmail.com>
2024-03-19 14:21:13 +0800

    login backend done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,9 +11,11 @@
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="../../index3.html" method="post">
+      <form method="POST" action="{{ route('login') }}">
+        @csrf
+
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" name="email" :value="old('email')" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -21,7 +23,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" name="password" required autocomplete="current-password" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
