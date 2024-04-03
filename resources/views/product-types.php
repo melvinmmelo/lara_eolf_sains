341c07c6a44f141c441c9e279a054264947ac5af
Olan <olan.andal@ub.edu.ph>
2024-04-03 09:20:15 +0800

    products module view ui done

---
+++ resources/views/product-types.php
@@ -0,0 +1,168 @@
+@extends('layouts.app')
+
+@section('contents')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Product Types</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Product Types</h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+
+            @include('layouts.errors')
+
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Code</th>
+                        <th>Name</th>
+                        <th>Volume</th>
+                        <th>Active</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>aaa</td>
+                        <td>aaa</td>
+                        <td>aaa</td>
+                        <td>aaa</td>
+
+                    </tr>
+
+                    <tr>
+                        <td>aaa</td>
+                        <td>aaa</td>
+                        <td>aaa</td>
+                        <td>aaa</td>
+
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Code</th>
+                        <th>Name</th>
+                        <th>Volume</th>
+                        <th>Active</th>
+
+                    </tr>
+                </tfoot>
+            </table>
+
+
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-product-types">
+                Add New
+            </button>
+        </div>
+        <!-- /.card-footer-->
+    </div>
+    <!-- /.card -->
+
+
+    <div class="modal fade" id="modal-product-types">
+        <div class="modal-dialog">
+            <form method="POST" action="{{ route('#') }}">
+                @csrf
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Add Product Types</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <div class="row mb-2">
+                                <div class="col-sm-3">
+                                    <label class="form-label" for="name">Code</label>
+                                    <input type="text" class="form-control" name="last_name">
+                                </div>
+                                <div class="col-sm-9">
+                                    <label class="form-label" for="name">Name</label>
+                                    <input type="text" class="form-control" name="first_name">
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="form-label" for="email">Volume</label>
+                                    <input type="text" class="form-control" name="email">
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label class="form-label" for="status">Active</label>
+                                    <br>
+                                    <input type="checkbox" id="mySwitch" data-bootstrap-switch data-on-text="Yes"
+                                        data-off-text="No" data-on-color="success" data-off-color="danger">
+
+                                    <div style="margin-bottom: 20px"></div>
+
+                                </div>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-success">Save
+                                    changes</button>
+                            </div>
+                        </div>
+
+
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-success">Save
+                                changes</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+
+        </form>
+    </div>
+    </div>
+
+</section>
+
+
+<!-- /.content -->
+@endsection
