ae2da8cae207f8d5e67c0065a9e207c8aecdd775
Melvin <melvinmmelo@gmail.com>
2024-07-12 17:00:15 +0800

    with loading ticket form

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Inbound;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class TicketController extends Controller
 {
@@ -11,8 +13,14 @@ class TicketController extends Controller
 
     public function generate()
     {
+        $ticketdetails="";
+        $sorted_product_codes = DB::table('product_types')->orderBy('sequence_no', 'asc')->select('code','spoon_pcs_per_bag')->get();
+        //dd($sorted_product_codes);
         $inbounds = Inbound::branch(session('branch_code'))->forLoading()->get();
-        return view('ticket.index', compact('inbounds'));
+        if(Session::has('ticketnum')){
+            $ticketdetails = Inbound::where('grp_print_ticket_no', session()->get('ticketnum'))->get();
+        }
+        return view('ticket.index', compact('inbounds','ticketdetails','sorted_product_codes'));
     }
 
 
@@ -33,9 +41,8 @@ class TicketController extends Controller
             $inbound->ticket_sequence_no = $cnt++;
             $inbound->save();
         }
+        session()->put('ticketnum', $grpPrintTicketNo);
 
-        dd("generated");
-
-        return redirect()->route('ticket.form', compact('inbounds', 'grpPrintTicketNo'));
+        return redirect()->route('generate-ticket');
     }
 }
