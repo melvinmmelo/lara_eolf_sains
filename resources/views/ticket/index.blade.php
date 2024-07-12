5cc0f06b9049e88cd75ad503c99d4c60d4528188
Melvin <melvinmmelo@gmail.com>
2024-07-12 19:08:13 +0800

    loading ticket multiple

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -102,102 +102,96 @@
     </section>
     <!-- /.content -->
 @endsection
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+@section("custom_js")
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script>
     $(document).ready(function() {
         try {
-            console.log("Document is ready");
             var curr_total=0;
             var last=0;
             var sp_count=0;
             var sp_set=0;
-            @php
-            if (Session::has('ticketnum')) {
-                $prod = json_decode($ticketdetails[0]->products);
-                //dd($prod);
-            @endphp
 
-            // Create a new window for printing
-            var mywindow = window.open('', 'PRINT', 'height=600,width=600');
-            if (!mywindow) {
-                console.log("Failed to open print window. Pop-up blocked?");
-                return;
-            }
+            @if(Session::has('ticketnum'))
+                // Create a new window for printing
+                var mywindow = window.open('', 'PRINT', 'height=600,width=600');
+                if (!mywindow) {
+                    console.log("Failed to open print window. Pop-up blocked?");
+                    return;
+                }
+                @foreach ($ticketdetails as $ticketdetail)
 
-            mywindow.document.write('<html><head><title>LOADING TICKET</title>');
-            mywindow.document.write('<style>');
-            mywindow.document.write('body { font-family: "Arial", Helvetica, sans-serif; font-size: 9pt; word-wrap: break-word; }');
-            mywindow.document.write('hr { border: none; border-top: 1px dotted black; }');
-            mywindow.document.write('td { font-family: "Arial", Helvetica, sans-serif; font-size: 9pt; word-wrap: break-word; }');
-            mywindow.document.write('</style>');
-            mywindow.document.write('</head><body>');
-            mywindow.document.write('<center>EOLF FOOD TRADING OPC</center><br>');
-            mywindow.document.write('<center>LOADING TICKET</center><br><br><br>');
-            mywindow.document.write('Sequence No: {{ $ticketdetails[0]->ticket_sequence_no }}<br>');
-            mywindow.document.write('Date: {{ $ticketdetails[0]->updated_at }}<br>');
-            mywindow.document.write('Delivery Person: (ID {{ $ticketdetails[0]->driver_id }}) {{ $ticketdetails[0]->driver->name }}<br>');
-            mywindow.document.write('Customer: {{ $ticketdetails[0]->customer->fullName }} ({{ $ticketdetails[0]->store->storename }})<br>');
-            mywindow.document.write('Encoded By: <br>');
-            mywindow.document.write('<table width="100%">');
-            mywindow.document.write('<tr><td>PRODUCT</td><td></td><td><center>QUANTITY</center></td><td align="right"></td></tr>');
+                    @php $prod = json_decode($ticketdetail->products); @endphp
 
-            @foreach ($sorted_product_codes as $sorted_code)
-            curr_total = 0;
-            last = 0;
-                @foreach ($prod as $product)
+                    mywindow.document.write('<html><head><title>LOADING TICKET</title>');
+                    mywindow.document.write('<style>');
+                    mywindow.document.write('body { font-family: "Arial", Helvetica, sans-serif; font-size: 9pt; word-wrap: break-word; }');
+                    mywindow.document.write('hr { border: none; border-top: 1px dotted black; }');
+                    mywindow.document.write('td { font-family: "Arial", Helvetica, sans-serif; font-size: 9pt; word-wrap: break-word; }');
+                    mywindow.document.write('</style>');
+                    mywindow.document.write('</head><body>');
+                    mywindow.document.write('<center>EOLF FOOD TRADING OPC</center><br>');
+                    mywindow.document.write('<center>LOADING TICKET</center><br><br><br>');
+                    mywindow.document.write('Sequence No: {{ $ticketdetail->ticket_sequence_no }}<br>');
+                    mywindow.document.write('Date: {{ $ticketdetail->updated_at }}<br>');
+                    mywindow.document.write('Delivery Person: (ID {{ $ticketdetail->driver_id }}) {{ $ticketdetail->driver->name }}<br>');
+                    mywindow.document.write('Customer: {{ $ticketdetail->customer->fullName }} ({{ $ticketdetail->store->storename }})<br>');
+                    mywindow.document.write('Encoded By: <br>');
+                    mywindow.document.write('<table width="100%">');
+                    mywindow.document.write('<tr><td>PRODUCT</td><td></td><td><center>QUANTITY</center></td><td align="right"></td></tr>');
 
-                    @if ($product->ptype_code == $sorted_code->code)
+                    @foreach ($sorted_product_codes as $sorted_code)
+                    curr_total = 0;
+                    last = 0;
+                        @foreach ($prod as $product)
+                            @if ($product->ptype_code == $sorted_code->code)
+                                mywindow.document.write('<tr><td>{{ $product->code }}</td><td></td><td style="text-align: center;">{{ $product->quantity }}</td><td style="text-align: right;"></td></tr>');
+                                curr_total += {{ $product->quantity }};
+                                sp_count += {{ $sorted_code->spoon_pcs_per_bag }} * {{ $product->quantity }};
+                                last=1;
+                            @else
+                                if(last==1){
+                                    last=1;
+                                }
+                                else{
+                                    last=0;
+                                }
+                            @endif
+                        @endforeach
+                            if(last==0){
 
-                        mywindow.document.write('<tr><td>{{ $product->code }}</td><td></td><td style="text-align: center;">{{ $product->quantity }}</td><td style="text-align: right;"></td></tr>');
-                        curr_total += {{ $product->quantity }};
-                        sp_count += {{ $sorted_code->spoon_pcs_per_bag }} * {{ $product->quantity }};
-                        last=1;
-
-                    @else
-                        if(last==1){
-                            last=1;
-                        }
-                        else{
-                            last=0;
-                        }
-
-                    @endif
-                @endforeach
-                    if(last==0){
-
-                    }
-                    else{
-                        mywindow.document.write('<tr><td></td><td></td><td></td><td align="left">[ '+curr_total +']</td></tr>');
-                mywindow.document.write('<tr><td colspan=4><hr></td></tr>');
-                    }
-                    sp_set=sp_count/12;
-
-            @endforeach
+                            }
+                            else{
+                                mywindow.document.write('<tr><td></td><td></td><td></td><td align="left">[ '+curr_total +']</td></tr>');
+                                mywindow.document.write('<tr><td colspan=4><hr></td></tr>');
+                            }
+                            sp_set=sp_count/12;
+                    @endforeach
 
 
-            mywindow.document.write('</table>');
+                    mywindow.document.write('</table>');
 
-            mywindow.document.write('<center>CUT FOR SPOON</center><br>');
-            mywindow.document.write('Sequence No: {{ $ticketdetails[0]->ticket_sequence_no }}<br>');
-            mywindow.document.write('Customer: {{ $ticketdetails[0]->customer->fullName }} ({{ $ticketdetails[0]->store->storename }})<br>');
-            mywindow.document.write('Total Spoon Count: '+sp_count+'<br>');
-            mywindow.document.write('Spoon Set: '+sp_set+'<br>');
-            mywindow.document.write('<hr>');
-            mywindow.document.write('</body></html>');
+                    mywindow.document.write('<center>CUT FOR SPOON</center><br>');
+                    mywindow.document.write('Sequence No: {{ $ticketdetail->ticket_sequence_no }}<br>');
+                    mywindow.document.write('Customer: {{ $ticketdetail->customer->fullName }} ({{ $ticketdetail->store->storename }})<br>');
+                    mywindow.document.write('Total Spoon Count: '+sp_count+'<br>');
+                    mywindow.document.write('Spoon Set: '+sp_set+'<br>');
+                    mywindow.document.write('<hr>');
+                    mywindow.document.write('</body></html>');
+                    @endforeach
 
-            mywindow.document.close(); // Necessary for IE >= 10
-            mywindow.focus(); // Necessary for IE >= 10
 
-            console.log("Printing...");
-            mywindow.print();
-            mywindow.close();
+                    mywindow.document.close();
+                    mywindow.focus();
 
-            @php
-             Session::forget('ticketnum');
-            }
-            @endphp
+                    mywindow.print();
+                    mywindow.close();
 
-            console.log("Print window closed");
+                @php Session::forget('ticketnum');  @endphp
+
+            @endif
+
         } catch (e) {
             console.error("Error in print script", e);
         }
