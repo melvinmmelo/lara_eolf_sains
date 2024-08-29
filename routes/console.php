c1c1de804854e66b6d67b1eefd43fd70098175b5
Melvin <melvinmmelo@gmail.com>
2024-08-29 17:05:57 +0800

    with backup db by spatie

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,15 @@
 <?php
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::call(function () {
-    DB::statement('INSERT INTO item_master_data_history SELECT * FROM item_master_data');
-})->dailyAt('23:59');
+// Schedule::call(function () {
+//     DB::statement('INSERT INTO item_master_data_history SELECT * FROM item_master_data');
+// })->dailyAt('23:59');
+
+
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
